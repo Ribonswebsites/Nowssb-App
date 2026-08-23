5748844f51e02b0f753c5657e706d4370e0061a3
Ribonswebsites <2.63561368e+08+Ribonswebsites@users.noreply.github.com>
2026-08-23 18:43:36 +0000

    Keep WidgetsPage route while removing Normal Home panel

--- flutter_app/lib/screens/home_normal.dart
+++ flutter_app/lib/screens/home_normal.dart
@@ -14,6 +14,7 @@ import '../shell/go.dart';
 import '../widgets/tv_frame.dart';
 import 'practice.dart' show nwsbSlotTitle;
 import 'sections.dart';
+import 'widgets_page.dart';
 import '../widgets/day_dashboard.dart';
 
 /// The website derives this from the hour in app/js/part026.js and the same
