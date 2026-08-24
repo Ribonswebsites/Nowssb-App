45602390ad29c23af73f866ddf537e4eb856d302
Ribonswebsites <2.63561368e+08+Ribonswebsites@users.noreply.github.com>
2026-08-24 14:52:48 +0000

    Restore latest WebView styles with August 15 panel

--- nowssb-update.js
+++ nowssb-update.js
@@ -7,8 +7,8 @@
   'use strict';
   var LIVE = 'https://raw.githubusercontent.com/Ribonswebsites/Nowssb-App/main/version.json';
   var PAGES = 'https://ribonswebsites.github.io/Nowssb-App/';
-  var LOCAL_VERSION = '9.6.22';
-  var LOCAL_BUILD = 2026082414;
+  var LOCAL_VERSION = '9.6.23';
+  var LOCAL_BUILD = 2026082415;
   var SEEN = 'nwsb_seen_build';
   var SNOOZE = 'nwsb_update_snooze';
 
