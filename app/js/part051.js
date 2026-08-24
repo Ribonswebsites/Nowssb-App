f03258cdbfa5b3d678b757b7b49aee21dd59a6c1
Ribonswebsites <2.63561368e+08+Ribonswebsites@users.noreply.github.com>
2026-08-24 20:13:03 +0000

    Remove remaining Cloudinary runtime fallbacks

--- app/js/part051.js
+++ app/js/part051.js
@@ -460,9 +460,6 @@
     if (!src) return '';
     var clean = src.split('#')[0].split('?')[0];
     if (/assets\/video\/[^/]+\.mp4$/i.test(clean)) return clean.replace(/\.mp4$/i, '-poster.webp');
-    if (clean.indexOf('res.cloudinary.com') >= 0 && clean.indexOf('/video/upload/') >= 0) {
-      return clean.replace('/video/upload/', '/image/upload/f_auto/').replace(/\.(mp4|webm|mov)$/i, '.jpg');
-    }
     return '';
   }
 
