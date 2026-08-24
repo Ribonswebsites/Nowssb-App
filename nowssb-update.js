7b191aca8d19329ccf5e935198b675c43225e656
Ribonswebsites <2.63561368e+08+Ribonswebsites@users.noreply.github.com>
2026-08-24 13:39:08 +0000

    Match WebView reference home geometry

--- nowssb-update.js
+++ nowssb-update.js
@@ -7,8 +7,8 @@
   'use strict';
   var LIVE = 'https://raw.githubusercontent.com/Ribonswebsites/Nowssb-App/main/version.json';
   var PAGES = 'https://ribonswebsites.github.io/Nowssb-App/';
-  var LOCAL_VERSION = '9.6.19';
-  var LOCAL_BUILD = 2026082411;
+  var LOCAL_VERSION = '9.6.20';
+  var LOCAL_BUILD = 2026082412;
   var SEEN = 'nwsb_seen_build';
   var SNOOZE = 'nwsb_update_snooze';
 
